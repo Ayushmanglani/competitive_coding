11dedc1738ad2a354fe5050442405e9f426bfd0b
Ayush_manglani <ayush.manglani@gmail.com>
2020-05-08 13:15:35 +0530

    Leetcode May 8

---
+++ leetcode/LC_M8_CheckIfStraightLine.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        x1 = coordinates[0][0]
+        y1 = coordinates[0][1]
+        x2 = coordinates[-1][0]
+        y2 = coordinates[-1][1]
+        try:
+            m = ((x2-x1)/(y2-y1))
+            # print(m)
+            if x2 == x1:
+                for i in range(1,len(coordinates)-1):
+                    if coordinates[i][0] != x1:
+                        return False
+            for i in range(1,len(coordinates)-1):
+                p = ((m)*(coordinates[i][1]-y1)) - (coordinates[i][0] - x1)
+                if p != 0.0:
+                    return False
+                    break
+            return True
+            
+                
+        except:
+            return 'Runtime Error'
