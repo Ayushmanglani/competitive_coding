d07b087f521b26330799d467a5ba7e4d3751829d
ayush_manglani <61349816+Ayushmanglani@users.noreply.github.com>
2020-06-03 23:19:51 +0530

    June 3

--- leetcode/June/J2_TwoCityScheduling.py
+++ leetcode/June/J2_TwoCityScheduling.py
@@ -1,24 +1,23 @@
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        s = 0
         l = len(costs)//2
         costs = sorted(costs, key=lambda i: abs(i[1]-i[0]))
-        n1 = n2 = 0
+        n1 = n2 = tcost = 0
         for i in range(len(costs)-1,-1,-1):
             if costs[i][0] < costs[i][1]:
                 if n1<l:
-                    s += costs[i][0]
+                    tcost += costs[i][0]
                     n1 += 1
                 else:
-                    s += costs[i][1]
+                    tcost += costs[i][1]
                     n2 += 1
             elif costs[i][0] > costs[i][1]:
                 if n2<l:
-                    s += costs[i][1]
+                    tcost += costs[i][1]
                     n2 += 1
                 else:
-                    s += costs[i][0]
+                    tcost += costs[i][0]
                     n1 += 1
             else:
-                s += costs[i][0]
-        return s
+                tcost += costs[i][0]
+        return tcost
