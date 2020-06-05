73e285159bc7d706d1b905ba6666898821fd0299
Ayush_manglani <ayush.manglani@gmail.com>
2020-06-06 00:41:16 +0530

    June 5

---
+++ leetcode/June/J5_RandomPick.py
@@ -0,0 +1,20 @@
+import random
+class Solution:
+    def __init__(self, w: List[int]):
+        self.prefix_sum = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sum.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        random_num = self.total_sum * random.random()
+        low, high = 0, len(self.prefix_sum)
+        while low < high:
+            mid = low + (high - low) // 2
+            if random_num > self.prefix_sum[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low
