d53e7ae78762c387bd44d595344b3bce166991fe
Ayushmanglani <ayush.manglani@gmail.com>
2020-06-30 01:23:27 +0530

    June 29

---
+++ leetcode/June/J29_UniquepathsInGrid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0 for _ in range(m)] for _ in range(n)]
+        
+        for i in range(m):
+            grid[0][i] = 1
+            
+        for i in range(n):
+            grid[i][0] = 1
+            
+        for r in range(1, n):
+            for c in range(1, m):
+                grid[r][c] = grid[r-1][c] + grid[r][c-1]
+                
+        return grid[n-1][m-1]
+        
+       
+        # if m==1 or n==1:
+        #     return(1)
+        # path = 1
+        # for i in range(n, (m + n - 1)): 
+        #     path *= i
+        #     path //= (i - n + 1)
+        # return path;
