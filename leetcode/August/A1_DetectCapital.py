b6df1ec96bb280a9730889f979cc85ce416a1485
Ayushmanglani <ayush.manglani@gmail.com>
2020-08-01 13:39:45 +0530

    August 1

---
+++ leetcode/August/A1_DetectCapital.py
@@ -0,0 +1,11 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if len(word) <= 1:
+            return True
+        if word.isupper():
+            return True
+        elif word[0].isupper() and word[1:].islower():
+            return True
+        elif word.islower():
+            return True
+        return False
