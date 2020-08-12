b55d479bf201ba840175bedd9514331ac8580e45
Ayushmanglani <ayush.manglani@gmail.com>
2020-08-12 20:27:38 +0530

    August 12

---
+++ leetcode/August/A12_Pascal'sTriangleII.py
@@ -0,0 +1,9 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        res = []
+        for i in range(0, rowIndex+1):
+            res.append([1]*(i+1))
+            for j in range(1, i):
+                res[i][j] = res[i-1][j-1] + res[i-1][j]            
+        print(res)
+        return res[rowIndex]
