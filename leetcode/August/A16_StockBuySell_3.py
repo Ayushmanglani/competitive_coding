956b796d282673113ff9408be926f36b4bf0cc9e
Ayushmanglani <ayush.manglani@gmail.com>
2020-08-16 23:59:05 +0530

    August 16

--- leetcode/August/A16_StockBuySell_3.py
+++ leetcode/August/A16_StockBuySell_3.py
@@ -12,3 +12,13 @@ class Solution:
                 highest = prices[i]
             maxProfit = max(maxProfit, (highest - prices[i]) + (profits[i-1] if i > 0 else 0))
         return maxProfit
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sell1, sell2, buy1, buy2 = 0, 0, -999999, -999999
+        for price in prices:
+            buy1 = max(buy1, -price)
+            sell1 = max(sell1, buy1 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell2 = max(sell2, buy2 + price)
+        return sell2        
