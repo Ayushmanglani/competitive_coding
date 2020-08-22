1be43931079c35e9903e9302bb4a29cd2fa860bb
Ayushmanglani <ayush.manglani@gmail.com>
2020-08-22 22:38:38 +0530

    August 22

--- leetcode/August/A22_NonOverlappingRect.py
+++ leetcode/August/A22_NonOverlappingRect.py
@@ -15,3 +15,22 @@ class Solution:
         x1, y1, x2, y2 = self.rects[i - 1]
         n -= self.ranges[i - 1]
         return [x1 + n % (x2 - x1 + 1), y1 + n // (x2 - x1 + 1)]
+
+
+class Solution:
+
+    def __init__(self, rects: List[List[int]]):
+        self.a = []
+        self.l = rects
+        cur = 0
+        for x in rects:
+            cur += (x[2] - x[0] + 1) *(x[3] - x[1] + 1)
+            self.a.append(cur)
+        print(self.a)
+
+    def pick(self) -> List[int]:
+        r = random.randint(1, self.a[-1])
+        for i,x in  enumerate(self.a):
+            if r <= x:
+                k = self.l[i]
+                return [random.randint(k[0], k[2]), random.randint(k[1], k[3])]        
