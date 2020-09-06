f5e0161c9d443f55c2e81bbe4b82035ff22203a9
Ayushmanglani <ayush.manglani@gmail.com>
2020-09-06 19:10:29 +0530

    Sept 6

--- leetcode/September/6_ImageOverLap.py
+++ leetcode/September/6_ImageOverLap.py
@@ -1,7 +1,6 @@
 class Solution:
     def largestOverlap(self, A: List[List[int]], B: List[List[int]]) -> int:
         n, m = len(A), len(A[0])
-        
         # keep track of the i,j coordinates where we have a 1 in both matrices: O(n**2)
         A1s = []
         B1s = []
@@ -11,8 +10,7 @@ class Solution:
                     A1s.append((i, j))
                 
                 if B[i][j] == 1:
-                    B1s.append((i, j))
-                    
+                    B1s.append((i, j))                    
         # for each i,j coordinate that is 1 in both A and B, find the delta between
         # the i and j coordinates (here by subtracting bi from ai and bj from aj)
         # and keep track of these: O(n**2)
@@ -23,7 +21,6 @@ class Solution:
                 if k in mem:
                     mem[k] += 1
                 else:
-                    mem[k] = 1
-                    
+                    mem[k] = 1                
         # return our max coordinate if we have on else 0: O(n)
         return max(mem.values() or [0])
