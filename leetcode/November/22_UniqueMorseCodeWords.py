6ea7c780e8c423fe616283e20357656f4cb69b67
Ayushmanglani <ayush.manglani@gmail.com>
2020-11-22 22:33:58 +0530

    Nov 22

---
+++ leetcode/November/22_UniqueMorseCodeWords.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        d = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        x = {}
+        output = 0
+        for word in words:
+            s = ""
+            for l in word:
+                s += d[ord(l)-97]
+            
+            if s not in x:
+                x[s] = True
+                output += 1
+        return output
