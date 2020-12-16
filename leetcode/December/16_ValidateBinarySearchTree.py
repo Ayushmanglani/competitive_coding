7b55846dbcad82d8ec406538d00e7a340bd55abf
Ayushmanglani <ayush.manglani@gmail.com>
2020-12-16 23:44:48 +0530

    Dec 16

--- leetcode/December/16_ValidateBinarySearchTree.py
+++ leetcode/December/16_ValidateBinarySearchTree.py
@@ -11,3 +11,21 @@ class Solution(object):
             return (check_bst(node.left, left, node.val) and check_bst(node.right, node.val, right))
         
         return check_bst(root, float("-inf"), float("inf"))
+
+class Solution(object):
+    def isValidBST(self, root):
+    
+        stack, inorder = [], float("-inf")      
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            
+            if root.val <= inorder:
+                return False
+            inorder = root.val
+            root = root.right
+            
+        return True        
