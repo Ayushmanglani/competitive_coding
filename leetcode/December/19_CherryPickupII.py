4a467cb0de56d1f8d548fc7cfaf99c5cc73a61a7
Ayushmanglani <ayush.m@gigindia.in>
2020-12-19 18:46:00 +0530

    Dec 19

--- leetcode/December/19_CherryPickupII.py
+++ leetcode/December/19_CherryPickupII.py
@@ -1,7 +1,6 @@
 class Solution:
     def cherryPickup(self, grid):
         M, N = len(grid[0]), len(grid)
-
         dp = [[[-10**9] * (M+2) for _ in range(M+2)] for _ in range(N)]
         dp[0][1][M] = grid[0][0] + grid[0][M-1]
         for j in range(1, N):
@@ -10,5 +9,4 @@ class Solution:
                 for shift1, shift2 in product([-1,0,1], [-1,0,1]):
                     cand_prev.append(dp[j-1][i1 + shift1][i2 + shift2])
                     dp[j][i1][i2] = (grid[j][i1-1] + grid[j][i2-1])//(1 + (i1 == i2)) + max(cand_prev)
-
         return max(list(map(max, *dp[-1]))) 
