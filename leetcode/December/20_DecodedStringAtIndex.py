9d70ba9d94ef9b8daefbd73fe702850d20e56211
Ayushmanglani <ayush.manglani@gmail.com>
2020-12-20 18:17:54 +0530

    Dec 20

--- leetcode/December/20_DecodedStringAtIndex.py
+++ leetcode/December/20_DecodedStringAtIndex.py
@@ -5,8 +5,29 @@ class Solution(object):
             l = l + 1 if c.isalpha() else l * int(c)
             stack += c,
             while l >= K:
-                while stack[-1].isdigit(): l //= int(stack.pop())
+                while stack[-1].isdigit(): 
+                    l //= int(stack.pop())
                 K = K % l
-                if not K: return stack[-1]
+                if not K: 
+                    return stack[-1]
                 l -= 1
                 stack.pop()
+
+class Solution(object):
+    def decodeAtIndex(self, S, K):
+        size = 0
+        for c in S:
+            if c.isalpha():
+                size += 1
+            else:
+                size *= int(c)
+        
+        for i in range(len(S)-1,-1,-1):
+            K = K % size
+            if K == 0 and S[i].isalpha():
+                return S[i]
+            
+            if S[i].isalpha():
+                size -= 1
+            else:
+                size /= int(S[i])                
