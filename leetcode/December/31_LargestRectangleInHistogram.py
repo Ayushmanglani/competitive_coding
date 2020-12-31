1c836bfbc198a10ce288c8b8fd1b8974d7cd77f1
Ayushmanglani <ayush.manglani@gmail.com>
2020-12-31 19:56:52 +0530

    Dec

---
+++ leetcode/December/31_LargestRectangleInHistogram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def largestRectangleArea(self, height):
+        height.append(0)
+        stack = [-1]
+        ans = 0
+        for i in xrange(len(height)):
+            while height[i] < height[stack[-1]]:
+                h = height[stack.pop()]
+                w = i - stack[-1] - 1
+                ans = max(ans, h * w)
+            stack.append(i)
+        height.pop()
+        return ans
