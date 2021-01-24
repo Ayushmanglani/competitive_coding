abe5b38db46a74d267d1395aaf1715c2797cac25
Ayushmanglani <ayush.manglani@gmail.com>
2021-01-24 21:11:26 +0530

    Jan 24

---
+++ leetcode/Jan_2021/23_MergekSortedLists.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def mergeKLists(self, lists):
+        self.nodes = []
+        head = point = ListNode(0)
+        for l in lists:
+            while l:
+                self.nodes.append(l.val)
+                l = l.next
+        for x in sorted(self.nodes):
+            point.next = ListNode(x)
+            point = point.next
+        return head.next
+
+from operator import attrgetter
+class Solution(object):
+    def mergeKLists(self, lists):
+        sorted_list = []
+        for head in lists:
+            curr = head
+            while curr is not None:
+                sorted_list.append(curr)
+                curr = curr.next
+
+        sorted_list = sorted(sorted_list, key=attrgetter('val'))
+        for i, node in enumerate(sorted_list):
+            try:
+                node.next = sorted_list[i + 1]
+            except:
+                node.next = None
+
+        if sorted_list:
+            return sorted_list[0]
+        else:
+            return None        
